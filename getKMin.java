3e412781d5ca75f4618c2158aff13bdc1396e8f0
bailerong <15929276532@163.com>
2019-04-20 22:54:55 +0800

    这是我们的寻找最小的第K个数的代码

--- getKMin.java
+++ getKMin.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
+/*这是我们的找出数组中第K个最小的数的代码*/
 public class getKMin {
+    /*采用剪枝法来判断我们的数组中的最小的第K个数*/
+    public static int quickSort(int a[],int begin,int end){
+        if(begin>end)
+            return Integer.MAX_VALUE;
+        int i=begin+1;
+        int j=end;
+        int temp=a[i];
+        while(i<j){
+            while(i<j&&a[j]>temp){
+
+            }
+        }
+    }
 }
